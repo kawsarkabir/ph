8cd7983efbc86cef00e60ce6a8a5d6a1a82b5e48
kawsarkabir <devkawsarkabir@gmail.com>
2023-10-14 22:19:15 +0600

    intro mongodb

---
+++ milestone 10/module  55- CRUD Operation/simple-crud-server/index.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 5000;
+
+// password and username
+/* 
+devkawsarkabir
+QQPXSEAjzOnj8sub
+
+*/
+
+// midleware
+app.use(cors());
+app.use(express.json());
+
+// mongose config
+const uri =
+  "mongodb+srv://devkawsarkabir:QQPXSEAjzOnj8sub@cluster0.hvlfmu8.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+
+
+   /*  app.post("/users", async (req, res) => {
+      const user = req.body;
+      console.log("new use", user);
+    }); */
+
+    // vai ar code
+    app.post("/user", async (req, res) => {
+        const user = req.body;
+        console.log("New user", user);
+        
+    });
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.log);
+
+app.get("/", (req, res) => {
+  res.send("SIMPLE CRUD OPERATION IS COMING");
+});
+
+app.listen(port, () => {
+  console.log(`SIMPLE CRUD OPERATION RUNING ON : ${port}`);
+});
